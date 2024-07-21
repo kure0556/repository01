9737fcfe8e3033f46a443dbcf46e41258bdd037f
Owner <Owner@DESKTOP-ERSLGS5>
2024-07-21 21:28:27 +0900

    テスト

--- zircuf-util/src/test/java/zircuf/core/business/validate/CheckerTest.java
+++ zircuf-util/src/test/java/zircuf/core/business/validate/CheckerTest.java
@@ -19,24 +19,27 @@ class CheckerTest {
 	@Test
 	void test() {
 		try {
+			CheckerTestDto checkerTestDto2 = new CheckerTestDto(null, null, " yy ", false, null, null);
+
 			CheckerTestDto checkerTestDto31 = new CheckerTestDto(null, null, " zz11 ", false, null, null);
 			CheckerTestDto checkerTestDto32 = new CheckerTestDto(null, null, " zz12", false, null, null);
 			CheckerTestDto checkerTestDto33 = new CheckerTestDto(null, null, "zz13 ", false, null, null);
 
-			CheckerTestDto checkerTestDto2 = new CheckerTestDto(null, null, " yy ", false, null, null);
 			CheckerTestDto checkerTestDto3 = new CheckerTestDto(" ", " zz1 ", " zz2 ", false, checkerTestDto2,
 					List.of(checkerTestDto31, checkerTestDto32, checkerTestDto33));
+
 			CheckerTestDto checkerTestDto1 = new CheckerTestDto("   aaa  ", null, "    cccc", false,
 					checkerTestDto2, Collections.singletonList(checkerTestDto3));
 
-			CheckerTestDto checkerTestDto4 = new CheckerTestDto("   aaa", " b ", "    cccc", false, null, null);
+			System.out.println(Validator.checkAndEdit(checkerTestDto1));
 
-			boolean check1 = Checker.of(CheckerTestDto.class).check(checkerTestDto1);
-			System.out.println("check1=" + check1);
-			boolean check2 = Checker.of(CheckerTestDto.class).check(checkerTestDto4);
-			System.out.println("check2=" + check2);
+			CheckerTestDto checkerTestDto5 = new CheckerTestDto("   555", " 5 ", "    5555", false, null, null);
+			CheckerTestDto checkerTestDto6 = new CheckerTestDto("   666", " 6 ", "  6666  ", false, null, null);
 
-			Editor.of(CheckerTestDto.class).edit(checkerTestDto1);
+			CheckerTestDto checkerTestDto4 = new CheckerTestDto("   444", " 4 ", "4444    ", false, checkerTestDto5,
+					Collections.singletonList(checkerTestDto6));
+
+			System.out.println(Validator.checkAndEdit(checkerTestDto4));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -60,7 +63,7 @@ class CheckerTest {
 		private boolean myField3;
 
 		@Deep
-		private CheckerTestDto myDto1;
+		private CheckerTestDto myDto;
 
 		@Edit(EType.NON_NULL_LIST)
 		@Deep
