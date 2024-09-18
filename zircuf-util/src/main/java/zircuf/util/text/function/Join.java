31d003b13e427369b9eff5ddfc3f94e2dee796c4
Owner <Owner@DESKTOP-ERSLGS5>
2024-09-18 21:56:57 +0900

    JOIN修正

--- zircuf-util/src/main/java/zircuf/util/text/function/Join.java
+++ zircuf-util/src/main/java/zircuf/util/text/function/Join.java
@@ -6,6 +6,16 @@ public final class Join {
 		return String.join(",", text);
 	}
 
+	public static final String csvDQ(String[] text) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : text) {
+            sb.append('"').append(s.replace("\"", "\"\"")).append('"').append(',');
+        }
+        // 最後のカンマを削除
+        sb.deleteCharAt(sb.length() - 1);
+		return String.join(",", text);
+	}
+
 	public static final String tsv(String[] text) {
 		return String.join("\t", text);
 	}
