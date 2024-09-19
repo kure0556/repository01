8cdde0c9560fabbfc29f8d5365b675de6416c584
Owner <Owner@DESKTOP-ERSLGS5>
2024-09-20 00:25:18 +0900

    コード生成

--- zircuf-util/src/main/java/zircuf/util/text/template/TextTemplate.java
+++ zircuf-util/src/main/java/zircuf/util/text/template/TextTemplate.java
@@ -1,8 +1,44 @@
 package zircuf.util.text.template;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * @param <D> テンプレートに注入するデータ形式
+ * @param <E> テンプレートに注入するデータ形式の注入処理
+ */
 interface TextTemplate<D, E extends TemplateItem<D>> {
+
+	static <T> TextTemplate<T, ?> compile(TextTemplate<T, ?> templateX, final String template) {
+		Pattern pattern = Pattern.compile("\\$\\{([^\\$\\{\\}]+)\\}");
+		Matcher m = pattern.matcher(template);
+		int i = 0;
+		while (i < template.length() && m.find(i)) {
+			if (i < m.start()) {
+				// マッチ箇所前までを抽出
+				String previus = template.substring(i, m.start());
+				templateX.addText(previus);
+			}
+			String key = m.group(1);
+			String defaultText = null;
+			if (key.contains(":")) {
+				// デフォルト値を抽出
+				String[] split = key.split(":", 2);
+				key = split[0];
+				defaultText = split[1];
+			}
+			// マッチ箇所を抽出
+			templateX.addParts(defaultText, key);
+			i = m.end();
+		}
+		if (i < template.length()) {
+			// 末尾までを抽出
+			String end = template.substring(i, template.length());
+			templateX.addText(end);
+		}
+		return templateX;
+	}
 
 	public List<E> itemList();
 
